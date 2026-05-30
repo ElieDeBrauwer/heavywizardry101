ace7a683e8f0927ec0218329e6282fe0dfec4fff
Elie De Brauwer <eliedebrauwer@gmail.com>
2026-05-30 15:49:29 +0200

    Dockerfile: enable sudo support with dropped privileges

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ USER_NAME=${CONTAINER_USER:-developer}
 
 groupadd -g $GROUP_ID $USER_NAME 2>/dev/null
 useradd --shell /bin/bash -u $USER_ID -g $GROUP_ID -G sudo -o -c "" -m $USER_NAME 2>/dev/null
+echo "$USER_NAME ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$USER_NAME
+chmod 0440 /etc/sudoers.d/$USER_NAME
 
 exec gosu $USER_NAME "$@"
