f526e62125901b023c6956da15f30da6f3a85010
Elie De Brauwer <eliedebrauwer@gmail.com>
2026-06-06 18:31:53 +0200

    Chapter 8 additions part one

--- code/08/snase.c
+++ code/08/snase.c
@@ -19,96 +19,90 @@
 
 FILE *flog;
 
-int
-run_code (int s)
-{
-  // Read in local buffer an copy in mmapped memory in child process
-  unsigned char *code;
-  int            len, off = 0;
-  
-  code = mmap (NULL,
-	       MEM_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC,
-	       MAP_ANON | MAP_PRIVATE, 0, 0);
-  
-  if (code == NULL) DIE ("mmap:");
-  
-  printf ("  +- Allocated %d Kb at %p\n", MEM_SIZE/1024, code);
-  fprintf (flog, "  +- Allocated %d Kb at %p\n", MEM_SIZE/1024, code);
-  fflush (flog);
-  
-  do {
-    len = read (s, code + off, MEM_SIZE);
-    off +=len;
-  } while (len == MEM_SIZE);
+int run_code(int s) {
+    // Read in local buffer and copy in mmapped memory in child process
+    unsigned char *code;
+    int len, off = 0;
 
-  
-  signal (SIGCHLD, SIG_IGN);  // Avoid Zombies
-  printf ("  +- Readed %d bytes from network.\n", len);
-  if ((fork () != 0)) {
-    munmap (code, MEM_SIZE); // Father has to release memory
-    return 0; // Father returns
-  }
-  else // Child runs shellcode
-    {
-      close (s);
-      printf ("  +- Ready to run Shellcode at %p code...\n", code);
-      fprintf (flog,"  +- Allocated %d Kb at %p\n", MEM_SIZE/1024, code);
-      fflush (flog);
-      
-      ((void(*)())code)();
-      
-      fprintf (flog, "  +- WoW... Shellcode returned control...");
-      fflush (flog);
-      
-      exit (EXIT_SUCCESS);
+    code = mmap(NULL,
+                MEM_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC,
+                MAP_ANON | MAP_PRIVATE, 0, 0);
+
+    if (code == NULL) DIE("mmap:");
+
+    printf("  +- Allocated %d Kb at %p\n", MEM_SIZE / 1024, code);
+    fprintf(flog, "  +- Allocated %d Kb at %p\n", MEM_SIZE / 1024, code);
+    fflush(flog);
+
+    do {
+        len = read(s, code + off, MEM_SIZE);
+        off += len;
+    } while (len == MEM_SIZE);
+
+
+    signal(SIGCHLD, SIG_IGN); // Avoid Zombies
+    printf("  +- Read %d bytes from network.\n", len);
+    if ((fork() != 0)) {
+        munmap(code, MEM_SIZE); // Father has to release memory
+        return 0; // Father returns
+    } else {
+        close(s);
+        printf("  +- Ready to run Shellcode at %p code...\n", code);
+        fprintf(flog, "  +- Allocated %d Kb at %p\n", MEM_SIZE / 1024, code);
+        fflush(flog);
+
+        ((void(*)()) code)();
+
+        fprintf(flog, "  +- WoW... Shellcode returned control...");
+        fflush(flog);
+
+        exit(EXIT_SUCCESS);
     }
-  
-  return 0;
+
+    return 0;
 }
 
-int
-main (int argc, char *argv[])
-{
-  int                  s, s1; // Server and service sockets
-  struct sockaddr_in   server;
-  struct sockaddr_in   client;
-  socklen_t               len = sizeof (struct sockaddr_in);
-  
-  printf ("SNASE's Not A Shellcode Executor\n");
-  printf ("Version " VERSION "\n");
-  printf ("(c) GBC Inc, 2024\n\n");
+int main(int argc, char *argv[]) {
+    int s, s1; // Server and service sockets
+    struct sockaddr_in server;
+    struct sockaddr_in client;
+    socklen_t len = sizeof(struct sockaddr_in);
 
-  if (argc != 1) {
-    fprintf (stderr, "Invalid number of parameters.\nUsage:\n\tsnsr\n");
-    exit (EXIT_FAILURE);
-  }
+    printf("SNASE's Not A Shellcode Executor\n");
+    printf("Version " VERSION "\n");
+    printf("(c) GBC Inc, 2024\n\n");
 
-  flog = fopen ("/tmp/snase.log", "a");
-  fprintf (flog, "%s", "SNASE Started up!\n");
-  fflush (flog);
-  // Create server socket
-  server.sin_addr.s_addr = INADDR_ANY;
-  server.sin_family = AF_INET;
-  server.sin_port = htons(PORT);
+    if (argc != 1) {
+        fprintf(stderr, "Invalid number of parameters.\nUsage:\n\tsnsr\n");
+        exit(EXIT_FAILURE);
+    }
 
-  if ((s = socket (AF_INET, SOCK_STREAM, 0)) < 0) DIE("socket:");
-  
-  /* OPTIONAL */  
-  /* Set reuse address/port socket option */
-  int ops = 1;
-  setsockopt (s, SOL_SOCKET, SO_REUSEADDR, &ops, sizeof(ops));
-  /* OPTIONAL */
-  
-  if (bind (s, (struct sockaddr *)&server, len) < 0) DIE ("bind:");
-  listen (s, 10);
-  
-  // On connection -> Run code
-  while (1) {
-    if ((s1 = accept (s, (struct sockaddr*) &client, &len)) < 0) DIE ("accept:");
-    printf ("> + Conection from %s\n", inet_ntoa (client.sin_addr));
-    fprintf (flog, "> + Conection from %s\n", inet_ntoa (client.sin_addr));
-    fflush (flog);
-    run_code (s1);
-    close (s1);
-  }
+    flog = fopen("/tmp/snase.log", "a");
+    fprintf(flog, "%s", "SNASE Started up!\n");
+    fflush(flog);
+    // Create server socket
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+
+    if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0) DIE("socket:");
+
+    /* OPTIONAL */
+    /* Set reuse address/port socket option */
+    int ops = 1;
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &ops, sizeof(ops));
+    /* OPTIONAL */
+
+    if (bind(s, (struct sockaddr *) &server, len) < 0) DIE("bind:");
+    listen(s, 10);
+
+    // On connection -> Run code
+    while (1) {
+        if ((s1 = accept(s, (struct sockaddr *) &client, &len)) < 0) DIE("accept:");
+        printf("> + Connection from %s\n", inet_ntoa(client.sin_addr));
+        fprintf(flog, "> + Connection from %s\n", inet_ntoa(client.sin_addr));
+        fflush(flog);
+        run_code(s1);
+        close(s1);
+    }
 }
