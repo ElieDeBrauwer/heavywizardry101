57187ff43f3b4e3ff170444d2f07cee53171bb37
Elie De Brauwer <eliedebrauwer@gmail.com>
2026-06-07 11:10:38 +0200

    Cleanup of extra directory

--- code/extra/sctw2kc.c
+++ code/extra/sctw2kc.c
@@ -5,41 +5,40 @@
 #include <unistd.h>
 
 
-int main (int argc, char *argv[]) {
-  char          buffer[1024], *p;
-  unsigned char mem[8];
-  int           l;
+int main(int argc, char *argv[]) {
+    char buffer[1024], *p;
+    unsigned char mem[8];
+    int l;
 
-  while (1) {
-    memset (buffer, 0, 1024);
-    memset (mem, 0, 8);
-    //if (read (0, buffer, 1024) <= 0) break;
-    if (fgets (buffer, 1024, stdin) == NULL) break;
-    buffer[strlen(buffer) - 1] = 0;
-    p = buffer;
-    l = 0;
-    fprintf (stderr, "1: Input: '%s'\n", buffer);
-    while (*p == ' ' || *p == '\t') p++;
-    fprintf (stderr, "2: Trimmed Input: '%s'\n", p);
-    if      (!strncasecmp (p, "NOP", 3))  mem[l++] = 0x90;
-    else if (!strncasecmp (p, "HALT", 4)) mem[l++] = 0xFF;
-    else if (!strncasecmp (p, "MOV", 3)) {
-      p+=3;
-      while (*p == ' ' || *p == '\t') p++;
-      if      (!strncasecmp (p, "EBX", 3)) mem[l++] = 0xbb;
-      else if (!strncasecmp (p, "EBP", 3)) mem[l++] = 0xbd;
-      p+=3;
-      while (*p == ' ' || *p == '\t' || *p == ',') p++;
-      int val = strtold (p, NULL);
-      mem[l++] = (unsigned char)(val & 0xff);
-    } else if (!strncasecmp (p, "ADD", 3)) {
-      mem[l++] = 0x01;
-      p+=3;
-      while (*p == ' ' || *p == '\t') p++;
-      if      (!strncasecmp (p, "EBX", 3)) mem[l++] = 0xeb;
-      else if (!strncasecmp (p, "EBP", 3)) mem[l++] = 0xea;      
+    while (1) {
+        memset(buffer, 0, 1024);
+        memset(mem, 0, 8);
+        //if (read (0, buffer, 1024) <= 0) break;
+        if (fgets(buffer, 1024, stdin) == NULL) break;
+        buffer[strlen(buffer) - 1] = 0;
+        p = buffer;
+        l = 0;
+        fprintf(stderr, "1: Input: '%s'\n", buffer);
+        while (*p == ' ' || *p == '\t') p++;
+        fprintf(stderr, "2: Trimmed Input: '%s'\n", p);
+        if (!strncasecmp(p, "NOP", 3)) mem[l++] = 0x90;
+        else if (!strncasecmp(p, "HALT", 4)) mem[l++] = 0xFF;
+        else if (!strncasecmp(p, "MOV", 3)) {
+            p += 3;
+            while (*p == ' ' || *p == '\t') p++;
+            if (!strncasecmp(p, "EBX", 3)) mem[l++] = 0xbb;
+            else if (!strncasecmp(p, "EBP", 3)) mem[l++] = 0xbd;
+            p += 3;
+            while (*p == ' ' || *p == '\t' || *p == ',') p++;
+            int val = strtold(p, NULL);
+            mem[l++] = (unsigned char) (val & 0xff);
+        } else if (!strncasecmp(p, "ADD", 3)) {
+            mem[l++] = 0x01;
+            p += 3;
+            while (*p == ' ' || *p == '\t') p++;
+            if (!strncasecmp(p, "EBX", 3)) mem[l++] = 0xeb;
+            else if (!strncasecmp(p, "EBP", 3)) mem[l++] = 0xea;
+        }
+        write(1, mem, l);
     }
-    write (1, mem, l);
-  }
-  
 }
--- code/extra/sctw2ke.c
+++ code/extra/sctw2ke.c
@@ -6,70 +6,72 @@
 #include <fcntl.h>
 
 unsigned char mem[1024];
-int           rip;
-int           ebx, ebp;
+int rip;
+int ebx, ebp;
 
-int sctw2k_status () {
-  printf ("EBX: 0x%x\n", ebx);
-  printf ("EBP: 0x%x\n", ebp);
-  printf ("RIP: 0x%x\n", rip);
-  puts ("--------------------------");
-  for (int i = 0; i < 16; i++) {
-    printf ("ADDR-0x%0x | %02x |", 15-i, mem[15-i]);
-    if (15 - i == rip) puts (" <= RIP"); else puts ("");
-  }
+void sctw2k_status() {
+    printf("EBX: 0x%x\n", ebx);
+    printf("EBP: 0x%x\n", ebp);
+    printf("RIP: 0x%x\n", rip);
+    puts("--------------------------");
+    for (int i = 0; i < 16; i++) {
+        printf("ADDR-0x%0x | %02x |", 15 - i, mem[15 - i]);
+        if (15 - i == rip) puts(" <= RIP");
+        else puts("");
+    }
 }
 
-int main (int argc, char *argv[]) {
-  char          buffer[1024], *p;
-  int           fd, l;
-  
-  printf ("SCTW-2000 Emulator\n");
-  if (argc != 2) {
-    fprintf (stderr, "Invalid number of parameters:\nUsage: %s input.bin\n",
-	     argv[0]);
-    exit (EXIT_FAILURE);
-  }
-  memset (mem, 0, 1024);
-  rip = ebx = ebp = 0;
-  // Read program in memory
-  if ((fd = open (argv[1], O_RDONLY)) < 0) {
-    perror ("open:");
-    exit (EXIT_FAILURE);
-  }
-  if (read(fd, mem, 1024) < 0) {
-    perror ("read:");
-    exit (EXIT_FAILURE);
-  }
-  close (fd);
-  
-  while (1) {
-    sctw2k_status ();
-  skip_status:    
-    printf ("%s", "> ");
-    if (fgets (buffer, 1024, stdin) == NULL) break;
-    buffer[strlen(buffer) - 1] = 0;
-    if (buffer[0] == 'q') exit (EXIT_SUCCESS);
-    if (buffer[0] == '?') {
-      puts ("------------------------------------");
-      puts ("? : Shows this help");
-      puts ("q : Quits program");
-      puts ("s : Instruction execution");
-      puts ("------------------------------------");
-      goto skip_status;
+int main(int argc, char *argv[]) {
+    char buffer[1024];
+    int fd;
+
+    printf("SCTW-2000 Emulator\n");
+    if (argc != 2) {
+        fprintf(stderr, "Invalid number of parameters:\nUsage: %s input.bin\n",
+                argv[0]);
+        exit(EXIT_FAILURE);
     }
-    if (buffer[0] == 's') {
-      if      (mem[rip] == 0x90) {rip++;continue;}
-      else if (mem[rip] == 0xff) exit (EXIT_SUCCESS);
-      else if (mem[rip] == 0xbb) ebx = mem[++rip];
-      else if (mem[rip] == 0xbd) ebp = mem[++rip];
-      else if (mem[rip] == 0x01) {
-	rip++;
-	if      (mem[rip] == 0xea) ebp += ebx;
-	else if (mem[rip] == 0xeb) ebx += ebp;
-      }
-      rip++;
+    memset(mem, 0, 1024);
+    rip = ebx = ebp = 0;
+    // Read program in memory
+    if ((fd = open(argv[1], O_RDONLY)) < 0) {
+        perror("open:");
+        exit(EXIT_FAILURE);
     }
-  }
-  
+    if (read(fd, mem, 1024) < 0) {
+        perror("read:");
+        exit(EXIT_FAILURE);
+    }
+    close(fd);
+
+    while (1) {
+        sctw2k_status();
+    skip_status:
+        printf("%s", "> ");
+        if (fgets(buffer, 1024, stdin) == NULL) break;
+        buffer[strlen(buffer) - 1] = 0;
+        if (buffer[0] == 'q') exit(EXIT_SUCCESS);
+        if (buffer[0] == '?') {
+            puts("------------------------------------");
+            puts("? : Shows this help");
+            puts("q : Quits program");
+            puts("s : Instruction execution");
+            puts("------------------------------------");
+            goto skip_status;
+        }
+        if (buffer[0] == 's') {
+            if (mem[rip] == 0x90) {
+                rip++;
+                continue;
+            } else if (mem[rip] == 0xff) exit(EXIT_SUCCESS);
+            else if (mem[rip] == 0xbb) ebx = mem[++rip];
+            else if (mem[rip] == 0xbd) ebp = mem[++rip];
+            else if (mem[rip] == 0x01) {
+                rip++;
+                if (mem[rip] == 0xea) ebp += ebx;
+                else if (mem[rip] == 0xeb) ebx += ebp;
+            }
+            rip++;
+        }
+    }
 }
